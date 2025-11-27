cb7ea360d551998f9104adb55c44b433701dc9bf
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2025-11-26 21:51:05 -0700

    serializer: added show_warnings suficiently

--- stardust/serializer.py
+++ stardust/serializer.py
@@ -41,7 +41,7 @@ def to_serial_dict(obj:Any) -> dict:
 	format_dict = {"name": "ganymede.Serializable", "version": SERIALIZER_FORMAT_VERSION}
 	return {"__serializer_format__": format_dict, "state": Serializable.serialize(obj)}
 
-def from_serial_dict(serial_data:dict) -> Any:
+def from_serial_dict(serial_data:dict, show_warnings:bool=False) -> Any:
 	""" Converts serialized-object data in dictionary format and returns a new
 	object of the specified type.
 	
@@ -51,7 +51,7 @@ def from_serial_dict(serial_data:dict) -> Any:
 	Returns:
 		New object
 	"""
-	return Serializable.deserialize(serial_data['state'])
+	return Serializable.deserialize(serial_data['state'], show_warnings=show_warnings)
 
 def restore_state(filename:str):
 	""" Returns an object that was serialized to dictionary format and reads
@@ -259,7 +259,7 @@ class Serializable:
 		return obj
 	
 	@staticmethod
-	def deserialize(obj:Any) -> Any:
+	def deserialize(obj:Any, show_warnings:bool=False) -> Any:
 		
 		def _decode_datetime(obj: dict) -> datetime:
 			s = obj["data"]
@@ -301,7 +301,10 @@ class Serializable:
 					if info.upgrade:
 						payload = info.upgrade(payload, from_version, to_version)
 				return info.from_(payload)
-
+			elif t:
+				if show_warnings:
+					print(f"Refraining to deserialize object of type {t} because it is not registered in the serializable class registry. Make sure relevant modules were imported.")
+			
 			# Ordinary dict
 			return {k: Serializable.deserialize(v) for k, v in obj.items()}
 
