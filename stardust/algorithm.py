ef9c02a63d5cd26c610dbf1b56be2f915893ea75
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2026-01-24 15:07:07 -0700

    added randrangef

--- stardust/algorithm.py
+++ stardust/algorithm.py
@@ -1,6 +1,7 @@
 import math
 import os
 import numpy as np
+import random
 
 def linstep(start, stop, step):
 	"""
@@ -50,4 +51,20 @@ def bounded_interp(x, y, x_target):
 	
 	if x_target < x[0] or x_target > x[-1]:
 		return None
-	return np.interp(x_target, x, y) 
+	return np.interp(x_target, x, y)
+
+def randrangef(xmin:float, xmax:float, step:float=None):
+	""" Returns a random number between 'xmin' and 'xmax' with
+	steps of 'step' enforced.
+	
+	if step = None, will not force any steps
+	"""
+	
+	# Get float value in range
+	value = random.random()*(xmax - xmin) + xmin
+	
+	# Round to a given step
+	if step is not None:
+		value = round(value/step)*step
+	
+	return value
