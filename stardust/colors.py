8b5ffdfc58ca4b5b5e33a159127c8ae3bbaccfe3
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2026-06-18 13:30:00 -0600

    Update colors.py

--- stardust/colors.py
+++ stardust/colors.py
@@ -6,3 +6,4 @@ defense_prussian_blue = (17/255, 69/255, 83/255)
 defense_light_blue = (220/255, 243/255, 254/255)
 
 modern_orange = (244/255, 148/255, 13/255)
+modern_red = (244/255, 13/255, 13/255)
